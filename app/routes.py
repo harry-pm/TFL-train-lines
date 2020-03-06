5507ddd88578810414d141e42c340dc5c37e5374
harry-pm <harrypenderedmazer@gmail.com>
2020-03-06 14:33:23 +0000

    get all lines at base url then get specific line with a query at status url

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,24 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 import requests
 
 @app.route('/')
 def index():
 
-    data = requests.get('https://api.tfl.gov.uk/Line/northern')
+    lines_request = requests.get('https://api.tfl.gov.uk/Line/Mode/tube').json()
 
-    print(data.text)
+    for i in range(len(lines_request)):
+        lines = lines_request[i]['id']
+
+    print(lines)
+
+    return render_template('home.html')
+
+@app.route('/status')
+def status_query():
+
+    line = request.args.get('line')
+
+    data = requests.get(f'https://api.tfl.gov.uk/Line/{line}/Status')
 
     return render_template('home.html', data = data.text)
