20ff1610d731daf1edb58718a2ea7d49c9e48eab
harry-pm <harrypenderedmazer@gmail.com>
2020-03-10 15:08:22 +0000

    implimented TFL ID and Key for actual data. Dropdown menu shows all the lines available. Submitting the form will fetch specific line data from the request arguments

--- app/routes.py
+++ app/routes.py
@@ -5,20 +5,21 @@ import requests
 @app.route('/')
 def index():
 
-    lines_request = requests.get('https://api.tfl.gov.uk/Line/Mode/tube').json()
+    # must create a base template with no jinja otherwise there will be errors as jinja will attempt to compute things that dont exist
 
-    for i in range(len(lines_request)):
-        lines = lines_request[i]['id']
-
-    print(lines)
-
-    return render_template('home.html')
+    return render_template('home.html',)
 
 @app.route('/status')
 def status_query():
+    
+    lines_request = requests.get('https://api.tfl.gov.uk/Line/Mode/tube?app_id=4216733b&app_key=1a60dd098dc91fca5006de8b92b72cee').json()
+
+    lines = []
+    for i in range(len(lines_request)):
+        lines.append(lines_request[i]['id'])
 
     line = request.args.get('line')
 
-    data = requests.get(f'https://api.tfl.gov.uk/Line/{line}/Status')
+    data = requests.get(f'https://api.tfl.gov.uk/Line/{line}/Status?app_id=4216733b&app_key=1a60dd098dc91fca5006de8b92b72cee')
 
-    return render_template('home.html', data = data.text)
+    return render_template('home.html', lines=lines, len=len(lines), data = data.text)
