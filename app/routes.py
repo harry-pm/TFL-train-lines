1e50f9147b1d7ba188dee8b8f4641dcef83bfc8e
harry-pm <harrypenderedmazer@gmail.com>
2020-03-21 23:20:10 +0000

    fetch tube lines upon opening the page and display json when submitting form

--- app/routes.py
+++ app/routes.py
@@ -2,24 +2,23 @@ from app import app
 from flask import render_template, request
 import requests
 
+lines_request = requests.get('https://api.tfl.gov.uk/Line/Mode/tube?app_id=4216733b&app_key=1a60dd098dc91fca5006de8b92b72cee').json()
+
+lines = []
+for i in range(len(lines_request)):
+    lines.append(lines_request[i]['id'])
+
 @app.route('/')
 def index():
 
-    # must create a base template with no jinja otherwise there will be errors as jinja will attempt to compute things that dont exist
+    return render_template('home.html', lines = lines)
 
-    return render_template('home.html',)
 
 @app.route('/status')
 def status_query():
     
-    lines_request = requests.get('https://api.tfl.gov.uk/Line/Mode/tube?app_id=4216733b&app_key=1a60dd098dc91fca5006de8b92b72cee').json()
-
-    lines = []
-    for i in range(len(lines_request)):
-        lines.append(lines_request[i]['id'])
-
     line = request.args.get('line')
 
     data = requests.get(f'https://api.tfl.gov.uk/Line/{line}/Status?app_id=4216733b&app_key=1a60dd098dc91fca5006de8b92b72cee')
 
-    return render_template('home.html', lines=lines, len=len(lines), data = data.text)
+    return render_template('home.html',  data = data.text, lines = lines)
